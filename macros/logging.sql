fb669c91a05afdffaed70aecfdb7c6fd2ba69278
Sanket Arora <arorasanket0101@gmail.com>
2024-05-16 10:56:21 +0530

    Initial commit for learning dbt

---
+++ macros/logging.sql
@@ -0,0 +1,8 @@
+{% macro learn_logging() %}
+    {{ log("Call your mom!") }}
+    {{ log("Call your dad!", info=True) }} --> Logs to the screen, too
+--  {{ log("Call your dad!", info=True) }} --> This will be put to the screen
+    {# log("Call your dad!", info=True) #} --> This won't be executed
+    {% set your_name_jinja = "Zoltan" %}
+    {{ log("Hello " ~ your_name_jinja, info=True) }}
+{% endmacro %}
